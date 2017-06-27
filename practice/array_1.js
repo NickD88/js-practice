47669953f7b208b46f512e1bc530f07086bf903d
Nick Dectis <ndectis@gmail.com>
2017-06-26 22:50:37 -0400

    work on js array problems

---
+++ practice/array_1.js
@@ -0,0 +1,26 @@
+// function lastInArray(array) {
+//   return array[array.length - 1];
+// }
+
+// console.log(lastInArray([1, 2, 3])) // 3
+// console.log(lastInArray(['a'])) // a
+
+// function rollCall(nameArray) {
+//   for (var i = 0; i < nameArray.length; i++) {
+//     console.log(nameArray[i]);
+//   }
+// }
+
+// var names = ['Abby', 'Mark', 'Sam', 'John'];
+// rollCall(names);
+
+// function reverseArray(arr) {
+//   reversedArray = [];
+//   for (var i = arr.length - 1; i >= 0; i--) {
+//     reversedArray.push(arr[i]);
+//   }
+
+//   return reversedArray;
+// }
+
+// console.log(reverseArray([1, 2, 3, 4, 5])); // [5, 4, 3, 2, 1]
