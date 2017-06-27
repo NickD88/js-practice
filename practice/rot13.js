cccbdc37c12245df514d1a7d670e044d88820178
Nick Dectis <ndectis@gmail.com>
2017-06-26 21:30:31 -0400

    refactor rot13

--- practice/rot13.js
+++ practice/rot13.js
@@ -1,44 +1,28 @@
 function rot13(string) {
   var cypherString = '';
-  var isUpperCase;
 
-  var encodeLowerCase = function(ascii) {
-    if (ascii + 13 > 122) {
-      var valuesFromBeginning = 13 - (122 - ascii);
-      return String.fromCharCode(96 + valuesFromBeginning);
+  var encodeChar = function(char) {
+    var cypherChar = char.charCodeAt() + 13;
+    var topZBoundary;
+
+    if (char.toLowerCase() === char) {
+      topZBoundary = 'z'.charCodeAt();
     } else {
-      return String.fromCharCode(ascii + 13);
+      topZBoundary = 'Z'.charCodeAt();
     }
-  }
-
-  var encodeUpperCase = function(ascii) {
-    if (ascii + 13 > 90) {
-      var valuesFromBeginnig = 13 - (90 - ascii);
-      return String.fromCharCode(64 + valuesFromBeginnig);
-    } else {
-      return String.fromCharCode(ascii + 13);
-    }
-  }
-
-  var encodeChar = function(ascii) {
-    var cypherChar = ascii + 13;
-    if (isUpperCase && cypherChar > 90) {
-      cypherChar -= 26;
-    } else if (!isUpperCase && cypherChar > 122) {
+    
+    if (cypherChar > topZBoundary) {
       cypherChar -= 26;
     }
-    return cypherChar;
+
+    return String.fromCharCode(cypherChar);
   }
 
   for (var i = 0; i < string.length; i++) {
     var asciiCode = string.charCodeAt(i);
 
-    if (asciiCode >= 65 && asciiCode <= 90) {
-      isUpperCase = true;
-      cypherString += encodeChar(asciiCode);
-    } else if (asciiCode >= 97 && asciiCode <= 122) {
-      isUpperCase = false;
-      cypherString += encodeChar(asciiCode);
+    if (string[i].match(/[a-zA-Z]/g)) {
+      cypherString += encodeChar(string[i]);
     } else {
       cypherString += string[i];
     }
@@ -56,6 +40,7 @@ console.log(rot13(rot13('Teachers open the door, but you must enter by yourself.
 
 
 // itterate over the string and build a new return string
-// determine if letter is uppercase or lowercase
-// add 13 to ascii value if value will be higher then z add remaining
-// to a value
+// determine if letter is uppercase or lowercase 
+// add 13 to ascii value 
+// if value the new value is higher then 'Z' or 'z' subtract 26 from total
+// concatinate the character represented by the ascii value to the new string
