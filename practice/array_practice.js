f295d1ba86fe289b48ad95eb1a3ad00ce34e266d
Nick Dectis <ndectis@gmail.com>
2017-07-13 07:43:54 -0400

    js practice exercises

---
+++ practice/array_practice.js
@@ -0,0 +1,99 @@
+// var myVar = 'This is global';
+
+// function someFunction() {
+//   var myVar = 'This is local'; // initialize new local function variable scope
+// }
+
+// someFunction();
+
+// console.log(myVar);  //will log the global variable 'This is global'
+
+
+// var myVar = 'This is global';
+
+// function someFunction() {
+//   var myVar = 'This is local';
+//   console.log(myVar);  // will log 'This is local'
+// }
+
+// someFunction();
+
+// var myVar = 'This is global';
+
+// function someFunction() {
+//   myVar = 'This is local';
+// }
+
+// someFunction();
+
+// console.log(myVar); // will log 'This is local'  variable is reassigned within the function
+
+// var myVar = 'This is global';
+
+// function someFunction() {
+//   console.log(myVar); //logs 'This is global'.  First checks local scope and then checks outer scopes until it reaches the global scope.
+// }
+
+// someFunction();
+
+// function someFunction() {
+//   myVar = 'This is global';
+// }
+
+// someFunction();
+// console.log(myVar); // logs 'This is global'  myVar declares in global scope 
+
+// var a = 7;
+
+// function myValue(b) { // the value of 'a' is passed into the function
+//  b += 10; // does not change the original a variable's value
+// }
+
+// myValue(a);
+// console.log(a); // will log 7 
+
+
+// var a = 7;
+
+// function myValue(a) { // establishes a local scope for the argument a
+//   a += 10; // does not reassign the global var a the new value
+// }
+
+// myValue(a);
+// console.log(a); // will log 7
+
+
+// var a = [1, 2, 3];
+
+// function myValue(b) { // passes the object's pointer into the function
+//   b[2] += 7; // this mutates the array's 2nd index position by adding 7 to the value
+// }
+
+// myValue(a);
+// console.log(a); // logs [1, 2, 10]
+
+// console.log(a); // logs undefined
+
+// var a = 1;
+
+//the variable declaration is hoisted to the top
+//var a
+//console.log(a);
+//a = 1;
+
+
+// logValue();  // will log 'Hello, world!'
+
+// function logValue() {  // the function declaration will be hoisted above function calls
+//   console.log('Hello, world!'); 
+// }
+
+
+var logValue = 'foo';
+
+function logValue() {
+  console.log('Hello, world!');
+}
+
+console.log(typeof logValue);
+
--- practice/js_basics.js
+++ practice/js_basics.js
@@ -32,6 +32,7 @@
 // console.log(number1 + ' % ' + number2 + ' = ' + (number1 % number2));
 // console.log(number1 + ' ** ' + number2 + ' = ' + Math.pow(number1, number2));
 
+// ********
 
 // Please write a phrase: walk
 // // console output
@@ -46,10 +47,96 @@
 
 // console.log('There are ' + phraseNoSpaces.length + ' characters in \"' + phraseNoSpaces + '\".') 
 
-function stringToInteger(str) {
+// var DIGITS = {'1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '0': 0}
+
+// function stringToInteger(str) {
+//     var output = 0
+//     var digits = digitArray(str);  
+
+//     for (var i = 0; i < digits.length; i++) {
+//       output = (10 * output) + digits[i]
+//     }
+
+//     return output;
+// }
+
+// function digitArray(str) {
+//   arrOutput = [];
+
+//   for (var i = 0; i < str.length; i++) {
+//     arrOutput.push(DIGITS[str[i]]);
+//   }
+//   return arrOutput;
+// }
+
+// console.log(stringToInteger('4321'));          // 4321
+// console.log(stringToInteger('570'));           // 570
+
+// ***************
+
+// function stringToSignedInteger(str) {
+//   var negNum = false;
+//   var signedOutput;
+
+//   if (str[0] === '-') {
+//     negNum = true;
+//     signedOutput = stringToInteger(str.slice(1));
+//   } else if (str[0] === '+') {
+//     signedOutput = stringToInteger(str.slice(1));
+//   } else {
+//     signedOutput = stringToInteger(str);
+//   }
+
+//   if (negNum) {
+//     signedOutput *= -1;
+//   }
+
+//   return signedOutput;
+// }
+
+// console.log(stringToSignedInteger('4321'));      // 4321
+// console.log(stringToSignedInteger('-570'));      // -570
+// console.log(stringToSignedInteger('+100'));      // 100
+
+DIGITS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+function integerToString(number) {
+  var strOutput = ''
+  var remainder;
   
+  do {
+    remainder = number % 10;
+    number = Math.floor(number / 10);
+
+    strOutput = DIGITS[remainder] + strOutput;
+
+  } while (number > 0);
+
+  return strOutput;
 }
 
+// console.log(integerToString(4321));    // "4321"
+// console.log(integerToString(0));       // "0"
+// console.log(integerToString(5000));    // "5000"
 
-stringToInteger('4321');          // 4321
-stringToInteger('570');           // 570
+
+function signedIntegerToString(number) {
+  var prefix = '';
+  var outputStr;
+
+  if (number < 0) {
+    prefix = '-'
+    outputStr = integerToString(number * - 1);
+  } else if (number === 0) {
+    outputStr = integerToString(number);
+  } else {
+    prefix = '+'
+    outputStr = integerToString(number);
+  }
+
+  return prefix + outputStr;
+}
+
+console.log(signedIntegerToString(4321));      // "+4321"
+console.log(signedIntegerToString(-123));      // "-123"
+console.log(signedIntegerToString(0));         // "0"
