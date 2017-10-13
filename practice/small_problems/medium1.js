8042854eb4c52875cc912fcb51c215f9a5f8e794
Nick <ndectis@gmail.com>
2017-10-13 15:41:43 -0400

    complete string-processing practice

--- practice/small_problems/medium1.js
+++ practice/small_problems/medium1.js
@@ -1,15 +1,15 @@
 // // Rotation Part 1
 
-// function rotateArray(arr) {
-//   var rotatedArr = [];
-//   if (Array.isArray(arr) === false) { return; }
-//   if (arr.length === 0) { return arr; }
-//   for (var i = 1; i < arr.length; i++) {
-//     rotatedArr.push(arr[i]);
-//   }
-//   rotatedArr.push(arr[0]);
-//   return rotatedArr;
-// }
+function rotateArray(arr) {
+  var rotatedArr = [];
+  if (Array.isArray(arr) === false) { return; }
+  if (arr.length === 0) { return arr; }
+  for (var i = 1; i < arr.length; i++) {
+    rotatedArr.push(arr[i]);
+  }
+  rotatedArr.push(arr[0]);
+  return rotatedArr;
+}
 
 // console.log(rotateArray(['a', 'b', 'c']));      // ['b', 'c', 'a']
 // console.log(rotateArray([7, 3, 5, 2, 9, 1]))   // [3, 5, 2, 9, 1, 7]
@@ -56,8 +56,115 @@ function maxRotation(num) {
   return parseInt(num, 10);
 }
 
-console.log(maxRotation(735291))        // 321579
-console.log(maxRotation(3))             // 3
-console.log(maxRotation(35))            // 53
-console.log(maxRotation(105))           // 15 # the leading zero gets dropped
-console.log(maxRotation(8703529146))    // 7321609845
+// console.log(maxRotation(735291))        // 321579
+// console.log(maxRotation(3))             // 3
+// console.log(maxRotation(35))            // 53
+// console.log(maxRotation(105))           // 15 # the leading zero gets dropped
+// console.log(maxRotation(8703529146))    // 7321609845
+
+
+// Stack Machine Interpretation
+
+/*
+input is a string of operations seperated by spacees
+output: number
+
+
+split the input string into array of operations
+itterate over the array and perform each operation
+use a case statement for each of the operation options
+
+*/
+function invalidNumStr(input) {
+  return input === '' || isNaN(input);
+}
+
+function validStack(stack) {
+  var VALID_TOKENS = ['PUSH', 'ADD', 'SUB', 'MULT', 'DIV', 'MOD', 'POP', 'PRINT']
+  var invalidTokens = stack.split(' ').filter(function(token) {
+    return VALID_TOKENS.indexOf(token) === -1 && invalidNumStr(token); 
+  }, []);
+  return invalidTokens.length === 0
+}
+
+
+function minilang(tokenStack) {
+  var stack = [];
+  var register = 0;
+  if (!validStack(tokenStack)) { return 'Invalid Entry' }
+  tokenStack.split(' ').forEach(function(operation) {
+    switch(operation) {
+      case 'PUSH':
+        stack.push(register);
+        break;
+      case 'ADD':
+        register += stack.pop();
+        break;
+      case 'SUB':
+        register -= stack.pop();
+        break;
+      case 'MULT':
+        register *= stack.pop();
+        break;
+      case 'DIV':
+        register = parseInt(register / stack.pop(), 10);
+        break;
+      case 'MOD':
+        register = parseInt(register % stack.pop(), 10);
+        break;
+      case 'POP':
+        register = stack.pop();
+        break;
+      case 'PRINT':
+        console.log(register);
+        break
+      default:
+        register = parseInt(operation, 10); 
+    }
+  });
+}
+// 5 3
+
+
+// console.log(minilang('PRINT'));
+// // 0
+
+// console.log(minilang('5 PUSH 3 MULT PRINT'));
+// // 15
+
+// console.log(minilang('5 PRINT PUSH 3 PRINT ADD PRINT'));
+// // 5
+// // 3
+// // 8
+
+// console.log(minilang('5 PUSH POP PRINT'));
+// // 5
+
+// console.log(minilang('3 PUSH 4 PUSH 5 PUSH PRINT ADD PRINT POP PRINT ADD PRINT'));
+// // 5
+// // 10
+// // 4
+// // 7
+
+// console.log(minilang('3 PUSH PUSH 7 DIV MULT PRINT '));
+// // 6
+
+// console.log(minilang('4 PUSH PUSH 7 MOD MULT PRINT '));
+// // 12
+
+// console.log(minilang('-3 PUSH 5 SUB PRINT'));
+// // 8
+
+// console.log(minilang('6 PUSH'))
+// // (nothing printed; no PRINT commands)
+
+
+// Word to Digit
+
+function wordToDigit(phrase) {
+
+}
+
+
+wordToDigit('Please call me at five five five one two three four. Thanks.');
+//'Please call me at 5 5 5 1 2 3 4. Thanks.'
--- practice/small_problems/string_processing.js
+++ practice/small_problems/string_processing.js
@@ -121,3 +121,84 @@ function staggeredCase2(str) {
 // console.log(staggeredCase2('I Love Launch School!'));     // 'I lOvE lAuNcH sChOoL!'
 // console.log(staggeredCase2('ALL CAPS'));                  // 'AlL cApS'
 // console.log(staggeredCase2('ignore 77 the 444 numbers')); // 'IgNoRe 77 ThE 444 nUmBeRs'
+
+// How Long are you
+
+function wordLengths(sentence) {
+  if (sentence.length === 0) { return []; }
+
+  var words = sentence.split(' ');
+  return words.map(function(word){
+    return word + ' ' + String(word.length);
+  });
+}
+
+
+// console.log(wordLengths('cow sheep chicken'));
+// // result
+// // ['cow 3', 'sheep 5', 'chicken 7']
+
+// console.log(wordLengths('baseball hot dogs and apple pie'));
+// // // result
+// // ['baseball 8', 'hot 3', 'dogs 4', 'and 3', 'apple 5', 'pie 3']
+
+// console.log(wordLengths('It ain\'t easy, is it?'));
+// // result
+// // ['It 2', 'ain\'t 5', 'easy, 5', 'is 2', 'it? 3']
+
+// console.log(wordLengths('Supercalifragilisticexpialidocious'));
+// // result
+// // ['Supercalifragilisticexpialidocious 34']
+
+// console.log(wordLengths(''));
+// // result
+[]
+
+
+// Search Word Part 1
+
+function searchWord(selection, text) {
+  if (arguments.length < 2 || selection.length === 0) {
+    return 0;
+  }
+
+  var reg = new RegExp(`\\b${selection}\\b`, 'gi');
+  var matches = text.match(reg);
+  return matches ? matches.length : 0;
+
+  // return text.split(' ').filter(function(word) {
+  //   return word.toLowerCase() === selection.toLowerCase();
+  // }).length;
+}
+
+
+// var text = 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?'
+// console.log(searchWord('sed', text)  === 3); // true
+
+
+// search word part 2
+
+function highlightWord(selection, text) {
+  if (arguments.length < 2 || selection.length === 0) {
+    return 0;
+  }
+
+  var reg = new RegExp(`\\b${selection}\\b`, 'gi');
+  return text.replace(reg, '**' + selection.toUpperCase() + '**')
+
+
+  // return text.split(' ').map(function(word) {
+  //   if (word.toLowerCase() === selection.toLowerCase()) {
+  //     return '**' + word.toUpperCase() + '**';
+  //   } else {
+  //     return word;
+  //   }
+  // }).join(' ');
+}
+
+
+// var text = 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?'
+
+// console.log(highlightWord('sed', text));
+// // result
+// '**SED** ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, **SED** quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, **SED** quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?'
