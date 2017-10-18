0544abd5cc776277e6eb803a18a54318394a2a66
Nick Dectis <ndectis@gmail.com>
2017-10-17 22:36:10 -0400

    js practice

--- practice/small_problems/advanced1.js
+++ practice/small_problems/advanced1.js
@@ -63,13 +63,13 @@ function transpose(matrixArr) {
 }
 
 
-var matrix = [
-  [1, 5, 8],
-  [4, 7, 2],
-  [3, 9, 6]
-];
+// var matrix = [
+//   [1, 5, 8],
+//   [4, 7, 2],
+//   [3, 9, 6]
+// ];
 
-var newMatrix = transpose(matrix);
+// var newMatrix = transpose(matrix);
 
 // console.log(newMatrix); // [[1, 4, 3], [5, 7, 9], [8, 2, 6]]
 // console.log(matrix);    // [[1, 5, 8], [4, 7, 2], [3, 9, 6]]
@@ -79,3 +79,41 @@ var newMatrix = transpose(matrix);
 // console.log(transpose([[1]]));                // [[1]]
 // console.log(transpose([[1, 2, 3, 4, 5], [4, 3, 2, 1, 0], [3, 7, 8, 6, 2]]));
 // [[1, 4, 3], [2, 3, 7], [3, 2, 8], [4, 1, 6], [5, 0, 2]]
+
+
+// Rotating Matricies
+
+
+function rotate90(matrix) {
+  var rotated = [];
+  for (var row = 0; row < matrix[0].length; row++) {
+    rotated.push([]);
+    for (var col = 0; col < matrix.length; col++) {
+      rotated[row][col] = matrix[(matrix.length - (col + 1))][row];
+    }
+  }
+  return rotated;
+}  
+
+var matrix1 = [
+  [1, 5, 8],
+  [4, 7, 2],
+  [3, 9, 6]
+];
+
+// 3, 4, 1
+// 9, 7, 5
+// 6, 2, 8
+
+var matrix2 = [
+  [3, 7, 4, 2],
+  [5, 1, 0, 8]
+];
+
+var newMatrix1 = rotate90(matrix1);
+var newMatrix2 = rotate90(matrix2);
+var newMatrix3 = rotate90(rotate90(rotate90(rotate90(matrix2))));
+
+console.log(newMatrix1); // [[3, 4, 1], [9, 7, 5], [6, 2, 8]]
+console.log(newMatrix2); // [[5, 3], [1, 7], [0, 4], [8, 2]]
+console.log(newMatrix3); // matrix2
